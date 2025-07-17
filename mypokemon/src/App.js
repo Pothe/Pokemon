43938b5fbcc17944e4121be08d05797db86c42bc
Pothe <keoutpothe2014@gmail.com>
2025-07-17 09:58:34 +0700

    Update App.js

--- mypokemon/src/App.js
+++ mypokemon/src/App.js
@@ -4,13 +4,23 @@ import axios from 'axios'
 function App() {
 
   const [pokemon, setPokem] = useState([])
+  const [currentPage, setCurrentPage]= useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPage, setNextPage] = useState()
+   const [prevPage, setprevPage] = useState()
+   const [loading, setloading]=useState(true)
 
-  useEffect(()=>{
- axios.get("https://pokeapi.co/api/v2/pokemon").then(res =>(
-    setPokem(res.data.results)
-  ))
-},[])
- 
+
+useEffect(()=>{
+ setloading(true);
+ axios.get(currentPage).then(res =>{ 
+  setloading(false);
+  setNextPage(res.data.next);
+  setprevPage(res.data.previous);
+  setPokem(res.data.results)
+})
+},[currentPage])
+
+ if(loading) return(<div>loadding...</div>)
   return (
   <Pokemonlis data={pokemon}/>
   );
