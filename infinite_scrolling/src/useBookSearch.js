c43ad2f6c928ad35054510c6f2c6d62dc92e09af
Pothe <keoutpothe2014@gmail.com>
2025-07-21 06:05:12 +0700

    infinit_scrolling

---
+++ infinite_scrolling/src/useBookSearch.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+export default function useBookSearch({query, pageNumber}) {
+    useEffect(()=>{
+        axios({
+            method:"GET",
+            url:"http://openlibrary.org/search.json",
+            params:{q: query, page: pageNumber}
+        })
+    },[query, pageNumber])
+  return
+}
