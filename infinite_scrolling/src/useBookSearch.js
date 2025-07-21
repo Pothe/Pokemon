659c97e26343bd25727a84028ffac41e32b2ea4d
Pothe <keoutpothe2014@gmail.com>
2025-07-21 08:57:41 +0700

    Update useBookSearch.js

--- infinite_scrolling/src/useBookSearch.js
+++ infinite_scrolling/src/useBookSearch.js
@@ -1,6 +1,5 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
-
 export default function useBookSearch({query, pageNumber}) {
     useEffect(()=>{
         axios({
@@ -8,6 +7,12 @@ export default function useBookSearch({query, pageNumber}) {
             URL:"http://openlibrary.org/search.json",
             params:{q:query, page:pageNumber}
         },[query, pageNumber])
+        .then(res=>{
+            console.log(res.data)
+        })
+        .catch(e=>{
+            return null
+        })
     })
   return
 }
