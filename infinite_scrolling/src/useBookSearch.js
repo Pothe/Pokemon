6160f2ff0de1e50beb539f74e52d6bd76697e7c1
Pothe <keoutpothe2014@gmail.com>
2025-07-21 14:52:32 +0700

    Update useBookSearch.js

--- infinite_scrolling/src/useBookSearch.js
+++ infinite_scrolling/src/useBookSearch.js
@@ -1,16 +1,40 @@
-import {useEffect } from 'react';
+import {useEffect} from 'react';
 import axios from 'axios';
 
-export default function useBookSearch({ query, pageNumber }) {
+export default function useBookSearch({ query, pageNumber }) { 
+  useEffect(() => {        
+//     axios.get("http://openlibrary.org/search.json", {
+//     params: { q: query, page: pageNumber }  })
+//      .then(res => {
+//     console.log(res.data);
+//   })   
+let Cancel
+axios({
+    method:"GET",
+    url:"http://openlibrary.org/search.json",
+    params:{q: query, page:pageNumber},
+    // clear previous data 
+    cancelToken: new axios.CancelToken(c=> Cancel =c)
+})
+// promise data
+.then(res=>{
+    console.log(res.data)
+})
+// to check error
+.catch(error => {
+      if (axios.isCancel(error)) {
+      
+        console.log('Request canceled:', error.message);
+      } else {
+        console.error(error);
+      }
+    });
 
-  useEffect(() => {     
-   
- axios.get("http://openlibrary.org/search.json", {
-    params: { q: query, page: pageNumber }
-  })
-  .then(res => {
-    console.log(res.data);
-  })
+    return ()=> Cancel()
+//   return () => {
+//     if (Cancel) Cancel('Cleanup: request canceled');
+//   };
+ 
 
   },[query, pageNumber]);
 
