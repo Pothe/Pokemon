b0dc8e8b8d439da8d999260e6aa923a5661fc35b
Pothe <keoutpothe2014@gmail.com>
2025-07-23 07:31:50 +0700

    Update hookingeff.js

--- hookings/src/Hooking/hookingeff.js
+++ hookings/src/Hooking/hookingeff.js
@@ -1,7 +1,7 @@
 import React,{useState ,useEffect} from 'react'
 
 function Hookingeff() {
-  const [resouretype, setresourcetype]= useState("posts")
+  const [resouretype, setresourcetype]= useState("users")
   const [iterm ,setiterm ] = useState([])
   const [datasource,setdatasource] = useState(true)
 
@@ -15,14 +15,15 @@ function Hookingeff() {
 
   return (
     <div>
+        <button onClick={()=>setresourcetype("users")}>users</button> 
      <button onClick={()=>setresourcetype("posts")}>Post</button> 
-     <button onClick={()=>setresourcetype("users")}>users</button> 
+   
      <button onClick={()=>setresourcetype("comments")}>Comment</button>
      <h1>{resouretype}</h1>
      {iterm.map(iterm => {
       return(
        <div key={iterm.id}>
-        <div key={iterm.id}> {datasource? iterm.title : iterm.name} </div>
+        <div> {iterm.name} </div>
        </div>
         
       ) 
