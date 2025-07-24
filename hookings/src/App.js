9d18212ffab89ea07dcd58ec7cea3d86af6ec6a3
Pothe <keoutpothe2014@gmail.com>
2025-07-24 11:13:28 +0700

    function context

--- hookings/src/App.js
+++ hookings/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import FunctionContextConponent from './FunctionContextConponent';
+export const ThemeContext = React.createContext()
 // import Hookingstate from './Hooking/Hookingstate';
-import Hooking from './Hooking/hookingeff';
-import JobBoard from './Hooking/Jobs';
-import Memo from './Hooking/memo';
+// import Hooking from './Hooking/hookingeff';
+// import JobBoard from './Hooking/Jobs';
+// import Memo from './Hooking/memo';
 
 
 function App() {
+  const [darkthem, setdarkthem] = useState('')
+  function ToggleClick(){
+    setdarkthem(prev=>!prev)
+  }
  
   return (
     <>
-    <Hooking/>
+    {/* <Hooking/>
     <JobBoard/>
-    <Memo/>
+    <Memo/> */}
     {/* <Hookingstate/> */}
+    <ThemeContext.Provider value={darkthem}>
+      <button onClick={ToggleClick} >Toogle</button>
+     <FunctionContextConponent/>
+    </ThemeContext.Provider>
     </>
 
 
---
+++ hookings/src/FunctionContextConponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FunctionContextConponent() {
+  return (
+    <div>FunctionContextConponent</div>
+  )
+}
+
+export default FunctionContextConponent
