66e7593520895e5eae7b73a8d5d08788f5a49057
Pothe <keoutpothe2014@gmail.com>
2025-07-24 16:41:55 +0700

    Update FunctionContextConponent.js

--- hookings/src/FunctionContextConponent.js
+++ hookings/src/FunctionContextConponent.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { ThemeContext } from './App'
 
 function FunctionContextConponent() {
+ 
+  const darkThem = useContext(ThemeContext)
+   const themeStyles ={
+    backgroundColor : darkThem? "" : '#333',
+    color: darkThem? '#333': '#ccc',
+    padding: '2rem',
+    margin: '2rem',
+    width:'100%',
+  }
   return (
-    <div>FunctionContextConponent</div>
+    <div style={themeStyles}>FunctionContextConponent</div>
   )
 }
 
